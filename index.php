7d7085d2ad096c4711a6f8b7af9f6c57226cb094
michele barlassina <michelebarlassina@MacBook-Pro-di-michele.local>
2018-06-20 00:06:48 +0200

    inital struct menu,footer,home

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <!-- meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/style.css">
+
+    <title>HomePage</title>
+</head>
+<body>
+<?php include('common/menu.html') ?>
+
+<main>
+    //slider con css.
+
+    Top 3 Piloti
+    News
+</main>
+
+<?php include('common/footer.html') ?>
+Link vari + Amministratori.
+
+</body>
+
+</html>
