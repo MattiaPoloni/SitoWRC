10cec0fb6dba520906788effed489c648cf74b64
michele barlassina <michelebarlassina@MacBook-Pro-di-michele.local>
2018-06-21 00:34:41 +0200

    header + menu

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
 <head>
     <!-- meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSS -->
     <link rel="stylesheet" href="/css/style.css">
 
     <title>HomePage</title>
+    <meta name="description" content="Descrizione sommaria.">
 </head>
 <body>
+<?php include('common/header.html') ?>
 <?php include('common/menu.html') ?>
 
 <main>
