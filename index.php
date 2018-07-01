b562e110b7f67abacf224de57c4436fb9b066791
michele barlassina <michelebarlassina@MacBook-Pro-di-michele.local>
2018-07-01 15:17:44 +0200

    add style + ingnore

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <!-- CSS -->
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/slide.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
     <title>HomePage</title>
     <meta name="description" content="Descrizione sommaria.">
@@ -16,34 +15,57 @@
 <body>
 <?php include('common/header.html') ?>
 <?php include('common/menu.html') ?>
+<div class="slider">
+    <ul>
+        <li>
+            <input type="radio" id="slide1" name="slide" checked>
+            <label for="slide1"></label>
+            <div class="cont-img" alt="panel 1"></div>
+        </li>
+        <li>
+            <input type="radio" id="slide2" name="slide">
+            <label for="slide2"></label>
+            <div class="cont-img-1" alt="panel 1"></div>
+        </li>
+        <li>
+            <input type="radio" id="slide3" name="slide">
+            <label for="slide3"></label>
+            <div class="cont-img" alt="panel 1"></div>
+        </li>
+        <li>
+            <input type="radio" id="slide4" name="slide">
+            <label for="slide4"></label>
+            <div class="cont-img-1" alt="panel 1"></div>
+        </li>
+    </ul>
+</div>
 
-<main>
-
-    <div class="slider">
-        <ul>
-            <li>
-                <input type="radio" id="slide1" name="slide" checked>
-                <label for="slide1"></label>
-                <div class="cont-img" alt="panel 1"></div>
-            </li>
-            <li>
-                <input type="radio" id="slide2" name="slide">
-                <label for="slide2"></label>
-                <div class="cont-img-1" alt="panel 1"></div>
-            </li>
-            <li>
-                <input type="radio" id="slide3" name="slide">
-                <label for="slide3"></label>
-                <div class="cont-img" alt="panel 1"></div>
-            </li>
-            <li>
-                <input type="radio" id="slide4" name="slide">
-                <label for="slide4"></label>
-                <div class="cont-img-1" alt="panel 1"></div>
-            </li>
-        </ul>
-    </div>
+<main class="row">
+    <!-- div padre -->
     <div class="container">
+
+    <div class="space"></div>
+        <!-- news -->
+        <div>
+        <?php
+        include("connessione.php");
+        $sql = "SELECT marca FROM Auto WHERE 1";
+        $results = mysqli_query($connessione, $sql);
+        $values = array();
+        while ($row = mysqli_fetch_array($results, MYSQLI_ASSOC))
+            $values[] = $row;
+        ?>
+        <?php foreach ($values as $value) : ?>
+            <article class="news-home">
+                <a href="" target="_self">
+                    <img src="" alt="">gs
+                    <time></time>
+                    <h3><?php echo stripslashes($value['marca']); ?></h3>
+                    <p></p>
+                </a>
+            </article>
+        <?php endforeach; ?>
+        </div>
         Top 3 Piloti
         News
         <br>
@@ -56,7 +78,6 @@
         <br>
 
     </div>
-
 </main>
 
 <?php include('common/footer.php') ?>
