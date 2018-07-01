88ddaeec3358f222eb305c5cb118269dced2612c
MatteoSantinon <40386435+MatteoSantinon@users.noreply.github.com>
2018-07-01 15:58:49 +0200

    Tolti Commenti

--- connessione.php
+++ connessione.php
@@ -5,16 +5,4 @@
         exit();
     }
 ?>
-<?php
-    /*function trovaLogo($nome) {
-        if(strpos($nome, 'Toyota') !== false)
-        $immagine = "<td><img class='logoAuto' src='media/toyota.png' alt='Logo Toyota'></td>";
-        if(strpos($nome, 'Ford') !== false)
-        $immagine = "<td><img class='logoAuto' src='media/ford.png' alt='Logo Ford'></td>";
-        if(strpos($nome, 'Citroen') !== false)
-        $immagine = "<td><img class='logoAuto' src='media/citroen.png' alt='Logo Citroen'></td>";
-        if(strpos($nome, 'Hyundai') !== false)
-        $immagine = "<td><img class='logoAuto' src='media/hyundai.png' alt='Logo Hyundai'></td>";
-        return $immagine;*/
-    }
-?>
+
