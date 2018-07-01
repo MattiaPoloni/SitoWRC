73fe1ae1e2949312b8623a393061ed60fc4ba7fc
MatteoSantinon <40386435+MatteoSantinon@users.noreply.github.com>
2018-07-01 12:18:25 +0200

    News Con Data

--- news.php
+++ news.php
@@ -17,27 +17,27 @@
 			if(isset($_POST["email"])) {
 				$connessione->query("INSERT INTO iscritto(email) VALUES ('".$_POST["email"]."');");
 			}
-			$query = "SELECT titolo, descrizione, fonte, indirizzo FROM Notizia ORDER BY id DESC;";
+			$query = "SELECT titolo, descrizione, fonte, indirizzo, data FROM Notizia ORDER BY id DESC;";
 			$ris = $connessione->query($query);
 			if(!$ris) {
                 echo "Errore Database";
             } else {
                 while($row = $ris->fetch_array(MYSQLI_NUM)) {
-						$link = "<a href='$row[3]'>Continea a leggere</a>";
-                        echo "<p><h2>$row[0]</h2> Fonte: $row[2]<br/>$row[1] $link</p>";
+						$link = "<a href='$row[3]'>Continua a leggere</a>";
+                        echo "<p><h2>$row[0]</h2> Data: $row[4] Fonte: $row[2]<br/>$row[1] $link</p>";
 				}
 			}
 			$connessione->close();
-
-
 		?>
 		<div>
 			<form action="news.php" method="post">
 				<fieldset>
+				<fieldset>
 					<legend>Iscriviti alla nostra Newsletter</legend>
             		<label for="email">E-mail:</label>
-            		<input name="email" id="email" maxlenght="30" />
-					<input type="submit" value="Invia" />
+            		<input name="email" id="email" maxlenght="30" />					
+					<input type="submit" value="Salva" />
+					<input type="reset" value="Cancella"/>
 				</fieldset>
 			</form>
 		</div>
