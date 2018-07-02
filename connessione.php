97e56e186e951467f66c0cf28c3d59b408619ad8
MatteoSantinon <40386435+MatteoSantinon@users.noreply.github.com>
2018-07-02 10:26:59 +0200

    Corretto Problema Caratteri

--- connessione.php
+++ connessione.php
@@ -1,5 +1,6 @@
 <?php
     $connessione = mysqli_connect("127.0.0.1", "root","","wrc");
+    mysqli_set_charset($connessione,"utf8");
     if($connessione->connect_errno) {
         echo "Connessione Fallita";
         exit();
