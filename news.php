f56475b7f878211880d856f5867a3ea6184d223d
MatteoSantinon <40386435+MatteoSantinon@users.noreply.github.com>
2018-07-02 10:04:59 +0200

    News Validato W3C

--- news.php
+++ news.php
@@ -1,15 +1,16 @@
-<html lang="it">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 <head>
     <!-- meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="News Dal Mondo WRC." />
     <!-- CSS -->
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
     <title>News</title>
-    <meta name="description" content="Descrizione sommaria.">
 </head>
 	<body>
+		<div id="news">
 		<?php
 			include('common/header.html');
 			include('common/menu.html');
@@ -24,18 +25,18 @@
             } else {
                 while($row = $ris->fetch_array(MYSQLI_NUM)) {
 						$link = "<a href='$row[3]'>Continua a leggere</a>";
-                        echo "<p><h2>$row[0]</h2> Data: $row[4] Fonte: $row[2]<br/>$row[1] $link</p>";
+                        echo "<p><h3>$row[0]</h3> <h5>Data: $row[4] Fonte: $row[2]</h5><br/><h6>$row[1] $link</h6></p>";
 				}
 			}
 			$connessione->close();
 		?>
+		</div>
 		<div>
 			<form action="news.php" method="post">
 				<fieldset>
-				<fieldset>
 					<legend>Iscriviti alla nostra Newsletter</legend>
             		<label for="email">E-mail:</label>
-            		<input name="email" id="email" maxlenght="30" />					
+            		<input name="email" id="email" />					
 					<input type="submit" value="Salva" />
 					<input type="reset" value="Cancella"/>
 				</fieldset>
