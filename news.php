326fc731962381e1ab689fd61d499a40518d11a0
MatteoSantinon <40386435+MatteoSantinon@users.noreply.github.com>
2018-07-03 16:05:25 +0200

    Update news.php

--- news.php
+++ news.php
@@ -2,11 +2,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it">
 <head>
     <!-- meta tags -->
-    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="description" content="News Dal Mondo WRC." />
     <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="css/style.css" />
+	<link rel="stylesheet" type="text/css" href="css/print.css" media="print">
     <title>News</title>
 </head>
 <body class="news">
