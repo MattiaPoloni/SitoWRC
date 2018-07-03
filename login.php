3aa1487f04039093a91205e42d80a98866a5f219
michele barlassina <michelebarlassina@MacBook-Pro-di-michele.local>
2018-07-03 18:18:04 +0200

    add DOCTYPE to login

--- login.php
+++ login.php
@@ -25,13 +25,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") :
     endif;
 endif;
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="it">
 <html>
 
 <head>
     <link rel="stylesheet" href="css/login.css">
-
-    <title>Pagina Login</title>
-
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="description" content="Login"/>
 </head>
 
 <body>
@@ -45,9 +47,11 @@ endif;
         <input type="submit" value=" Submit "/><br/>
     </form>
 
-    <?php if($flag === true) : ?>
+    <?php if ($flag === true) : ?>
         <p>Nome o Password invalidi.</p>
     <?php endif; ?>
+
+    <p>Non sei un amministratore?<br/> Torna all'<a href="index.php">HOME</a> per poter leggere le più interessanti news.</p>
 </main>
 
 </body>
