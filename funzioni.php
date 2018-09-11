be693e5102988dc486c17bb98b214e98a35ff5c9
MatteoSantinon <40386435+MatteoSantinon@users.noreply.github.com>
2018-09-11 13:00:51 +0200

    piccolo bug

--- funzioni.php
+++ funzioni.php
@@ -1,13 +1,13 @@
 <?php
 function trovaLogo($nome) {
     if(strpos($nome, 'Toyota') !== false)
-        $immagine = "<td><img class='logoAuto' src='media/toyota.png' alt='Logo Toyota'></td>";
+        $immagine = "<td><img class='logoAuto' src='media/toyota.png' alt='Logo Toyota' /></td>";
     if(strpos($nome, 'Ford') !== false)
-        $immagine = "<td><img class='logoAuto' src='media/ford.png' alt='Logo Ford'></td>";
+        $immagine = "<td><img class='logoAuto' src='media/ford.png' alt='Logo Ford' /></td>";
     if(strpos($nome, 'Citroen') !== false)
-        $immagine = "<td><img class='logoAuto' src='media/citroen.png' alt='Logo Citroen'></td>";
+        $immagine = "<td><img class='logoAuto' src='media/citroen.png' alt='Logo Citroen' /></td>";
     if(strpos($nome, 'Hyundai') !== false)
-        $immagine = "<td><img class='logoAuto' src='media/hyundai.png' alt='Logo Hyundai'></td>";
+        $immagine = "<td><img class='logoAuto' src='media/hyundai.png' alt='Logo Hyundai' /></td>";
     return $immagine;
 }
 
