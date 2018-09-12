8971f8a793385e0bc63181c1dd53033466938da4
michele barlassina <michelebarlassina@MacBook-Pro-di-michele.local>
2018-09-12 18:20:06 +0200

    validate img

--- common/footer.php
+++ common/footer.php
@@ -27,7 +27,14 @@
                 <a href="admin.php?azione=default" tabindex="11">Amministratore</a>
             <?php endif; ?>
         </div>
-        <p>
+        <p class="valid">
+            <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                <img style="border:0;width:88px;height:31px"
+                     src="http://jigsaw.w3.org/css-validator/images/vcss"
+                     alt="CSS Valido!" />
+            </a>
+        </p>
+        <p class="valid">
             <a href="http://validator.w3.org/check?uri=referer"><img
                         src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" /></a>
         </p>
